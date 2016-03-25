4435223d8292b58cb5d3220d153e568c83a7d666
Toni Pikkarainen <toni.t.pikkarainen@gmail.com>
2016-03-25 16:26:56 +0200

    uudet angular sivut

---
+++ app.js
@@ -0,0 +1,37 @@
+﻿(function() {
+  var app = angular.module('gemStore', ['store-directives']);
+
+ app.controller('StoreController', ['$http', function($http){
+  var store = this;
+  store.products = [];
+
+  $http.get('/store-products.json').success(function(data){
+    store.products = data;
+  });
+}]);
+  
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+  
+  app.controller('ReviewController', function(){
+    this.review={};
+    this.addReview=function(product){
+	  this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review={};
+    };
+  });
+  
+ 
+ 
+})();
--- application.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function(){
-	    var teksti = $('<p> Miltä tuntui?</p>');
-		$(".nappi").on("click",function(){
-		$("#tunnus").append(teksti);
-		$(".nappi").remove();
-		});
-});
