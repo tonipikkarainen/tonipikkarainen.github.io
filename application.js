398e4020b1e1c20edf4073c252e707ab3376b43d
Toni Pikkarainen <toni.t.pikkarainen@gmail.com>
2016-10-14 08:55:28 +0300

    monikulmioala

--- application.js
+++ application.js
@@ -8,4 +8,10 @@ $(document).ready(function(){//TODO: tee silmukka!!
 		$(".vastausnappi3").on("click",function(){
 		$("#vastausId3").toggleClass('hidden');
 		});
+		$(".vastausnappi4").on("click",function(){
+		$("#vastausId4").toggleClass('hidden');
+		});
+				$(".vastausnappi5").on("click",function(){
+		$("#vastausId5").toggleClass('hidden');
+		});
 });
